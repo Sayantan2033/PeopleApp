da0f85b4d605eecb17922da612a732f8a71659b4
Sayantan2033 <sayantansaha2033@gmail.com>
2016-07-05 19:40:59 +0530

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,26 @@
+<?php
+    $con = mysqli_connect("mysql8.000webhost.com", "a9754933_yash1", "12345678yash", "a9754933_Yash");
+    
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+    $statement = mysqli_prepare($con, "SELECT * FROM user WHERE username = ? AND password = ?");
+    mysqli_stmt_bind_param($statement, "ss", $username, $password);
+    mysqli_stmt_execute($statement);
+    
+    mysqli_stmt_store_result($statement);
+    mysqli_stmt_bind_result($statement, $userID, $name, $age, $username, $password);
+    
+    $response = array();
+    $response["success"] = false;  
+    
+    while(mysqli_stmt_fetch($statement)){
+        $response["success"] = true;  
+        $response["name"] = $name;
+        $response["age"] = $age;
+        $response["username"] = $username;
+        $response["password"] = $password;
+    }
+    
+    echo json_encode($response);
+?>
---
+++ Register.php
@@ -0,0 +1,17 @@
+<?php
+    $con = mysqli_connect("mysql8.000webhost.com", "a9754933_yash1", "12345678yash", "a9754933_Yash");
+    
+    $name = $_POST["name"];
+    $age = $_POST["age"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $statement = mysqli_prepare($con, "INSERT INTO user (name, age, username, password) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "siss", $name, $age, $username, $password);
+    mysqli_stmt_execute($statement);
+    
+    $response = array();
+    $response["success"] = true;  
+    
+    echo json_encode($response);
+?>
